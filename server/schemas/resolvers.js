1cbbc5918bec9123be28abbbba23a772dd4ddce4
William Skillas <wmskillas@gmail.com>
2022-05-29 19:29:42 -0400

    finished resolvers and updated typeDefs to include the Query and Mutation as well as adding the Auth that I missed before

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,20 +1,67 @@
-const { Book, User } = require("../models");
+const { User } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
-    user: async (parent, args, { user }) => {
+    me: async (parent, args, { user }) => {
       if (user) {
         const params = await User.findOne({ _id: user._id }).select(
           "-__v -password"
         );
         return params;
       }
+      throw new AuthenticationError("You are not logged in...");
     },
   },
   Mutation: {
-    createUser: async (parent, args) => {
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("Wrong Email or Password...");
+      }
+
+      const userPassword = await user.isCorrectPassword(password);
+      if (!userPassword) {
+        throw new AuthenticationError("Wrong Email or Password...");
+      }
+
+      const token = signToken(user);
+      return { user, token };
+    },
+
+    addUser: async (parent, args) => {
       const user = await User.create(args);
-      return user;
+      const token = signToken(user);
+      return { user, token };
+    },
+
+    saveBook: async (parent, { bookData }, { user }) => {
+      if (user) {
+        const updateUserBooks = await User.findOneAndUpdate(
+          { _id: user._id },
+          { $push: { savedBooks: bookData } },
+          { new: true }
+        );
+        return updateUserBooks;
+      }
+      throw new AuthenticationError(
+        "You must be signed in to change this content..."
+      );
+    },
+    removeBook: async (parent, { bookId }, { user }) => {
+      if (user) {
+        const removeUserBook = await User.findOneAndUpdate(
+          { _id: user._id },
+          { $pull: { savedBooks: { bookId } } },
+          { new: true }
+        );
+        return removeUserBook;
+      }
+      throw new AuthenticationError(
+        "You must be signed in to change this content..."
+      );
     },
   },
 };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,22 +1,35 @@
-const { gql } = require('apollo-server-express')
+const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-type Book {
-    _id: ID!
-    authors: [String!]
-    description: String!
-    bookId: String!
-    image: String!
-    link: String!
-    title: String!
-}
-type User {
+  type Query {
+    me: User
+  }
+  type Mutations {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: [BookInput!]): User
+    removeBook(bookId: ID!): User
+  }
+  type User {
     _id: ID!
     username: String!
-    email: String!
-    password: String!
-    saveBooks: [Books]
-}
+    email: String
+    saveBooks: [Book]
+  }
+
+  type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    image: String
+    link: String
+    title: String!
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
 `;
 
 module.exports = typeDefs;
